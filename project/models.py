3648c35b4c7b476bdd38f16f6a443fd0e4378343
olakunle <akandebaba48@gmail.com>
2024-03-17 16:27:50 +0100

    created task and project models

--- project/models.py
+++ project/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
 # Create your models here.
+class Project (models.Model):
+    text = models.CharField(max_length=240)
+    date = models.DateTimeField(auto_now_add=True)
+    image = ImageField()
--- task/models.py
+++ task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from project.models import Project
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=240)
+    completed = models.BooleanField()
+    pending = models.BooleanField()
+    
+    project_id = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
