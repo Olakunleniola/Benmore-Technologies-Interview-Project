8682ac103f53887d488c6f81b5746e207ba19bb3
olakunle <akandebaba48@gmail.com>
2024-03-17 17:19:55 +0100

    registered the models in their respective admin.py file

--- task/models.py
+++ task/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from project.models import Project
 
-# Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=240)
-    completed = models.BooleanField()
-    pending = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    pending = models.BooleanField(default=True)
     
     project_id = models.ForeignKey(
         Project,
