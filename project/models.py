eb2c23e4c6f25e697658d6b52a5774d46c5ba7e8
olakunle <akandebaba48@gmail.com>
2024-03-18 21:43:22 +0100

    added an str method to handle string representation of the Project model

--- project/models.py
+++ project/models.py
@@ -5,3 +5,6 @@ class Project (models.Model):
     title = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now_add=True)
     image = ImageField()
+    
+    def __str__(self):
+        return self.title[0:20]
