bf17e3a482a7f9d0e404f75cb1f7319be5caaae6
olakunle <akandebaba48@gmail.com>
2024-03-19 00:33:47 +0100

    Implement AJAX requests for project and task management
    - Added AJAX request calls to interact with the API endpoints for creating, modifying, deleting, and retrieving projects and tasks.
    - Implemented frontend functionality to handle form submissions and display real-time updates.
    - Updated API endpoints to handle CRUD operations for projects and tasks.
    - Tested the AJAX requests and API endpoints to ensure proper functionality and data consistency.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-// Handle CRSF TOken 
+// Handle Django CSRF Token for submission of forms
 $.ajaxSetup({
     beforeSend: function beforeSend(xhr, settings) {
         function getCookie(name) {
@@ -29,16 +29,72 @@ $.ajaxSetup({
     },
 });
 
-
-
+// Toogle Create New Project Modal 
 $(document).on("click", ".toggle-project-modal", function(e) {
     e.preventDefault()
     $(".project-modal").toggleClass("hidden")
     // #TODO: clear file in file input
 })
 
-//  ...........Create New Project.........
+// Toogle Create new Task Modal
+.on("click", ".toggle-task-modal", function(e){
+    $(".task-modal").toggleClass("hidden");
+    $(".js-task-submit").data("parentid", $(this).data("parentid"))
+    $(".js-task-submit").data("url",$(this).data("url"))
+})
 
+//  .........Create New Task............
+.on("click", ".js-task-submit", function(e){
+    const $btn = $(this);
+    const title = $(".task-title").val().trim();
+    const description = $(".task-description").val().trim();
+    const buttonInnerHtml = $btn.html(); 
+    const completed_task = $('#completed_task').prop("checked");
+    const $parent = $(`#${$btn.data("parentid")}`);
+    
+    if(!title.length){
+        $(".task-title").addClass("red_shadow placeholder:text-red-400").prop("placeholder", "This field is required")
+        setTimeout(() => {
+            $(".task-title").removeClass("red_shadow placeholder:text-red-400").prop("placeholder", "Enter Task Title")
+        }, 5000)
+        return
+    }
+    
+
+    $btn.prop("disabled", true).text("Creating....");
+
+    $.ajax({
+        type: "POST",
+        url: $btn.data("url"),
+        data: {
+            title: title,
+            description: description,
+            completed_task: completed_task
+        },
+        
+        success: (htmlData) => {
+            // console.log(htmlData)    
+            $(".task-modal").addClass("hidden");
+            $btn.prop("disabled", false).html(buttonInnerHtml)
+            $(".task-description").val("");
+            $('.task-title').val("");
+            $parent.html(htmlData);
+        },
+
+        error: (error) => {
+            console.warn(error.responseJSON);
+            $btn.addClass("bg-red-500 text-white");
+            $btn.prop("diabled", false).text("Error").css("background-color", "red");
+            setTimeout(() => {
+                $btn.removeClass("bg-red-500 text-white");
+                $btn.prop("disabled", false).html(buttonInnerHtml).css("background-color", "white")
+                $(".project-modal").addClass("hidden")
+            }, 5000)
+        }
+    }) 
+})
+
+//  ...........Create New Project.........
 .on("click", ".js-project-submit", function(e){
     e.preventDefault();
     const $btn = $(this);
@@ -46,10 +102,14 @@ $(document).on("click", ".toggle-project-modal", function(e) {
     const text = $textarea.val().trim();
     const buttonInnerHtml = $btn.html(); 
     const imageFile = $('#new_upload').prop("files")[0];
-    
-    console.log(text)
 
-    if(!text.length){return}
+    if(!text.length){
+        $(".project-name").addClass("red_shadow placeholder:text-red-400").prop("placeholder", "This field is required")
+        setTimeout(() => {
+            $(".project-name").removeClass("red_shadow placeholder:text-red-400").prop("placeholder", "Enter Project Title/Name......")
+        }, 5000)
+        return
+    }
     
     const formData = new FormData();
     formData.append('title', text);
@@ -89,27 +149,32 @@ $(document).on("click", ".toggle-project-modal", function(e) {
 })
 
 
-
+//  Search Project by Title/Name
 .on("click", "#search_botton", function(e) {
+    e.preventDefault()
     const $searchBar = $(".search-bar")
-    const query = $searchBar.val()
+    const query = $searchBar.val().trim()
     if (!query){return}
-    console.log(query) 
     
     $.ajax ({
         type: "GET",
-        url: $(this).data("url"),
-        data: query,
+        url: `/?query=${query}`,
 
-        success: (data) => {
-            console.log(data)
+        success: (htmlData) => {
+            $searchBar.val("")
+            $('#result_container').html(htmlData)
         },
 
         error: (e) => {
-            console.log(e)
             $searchBar.addClass("text-red-500 font-bold")
             $(".search-div").addClass("red_shadow")
-            $searchBar.val("OOpsss!!!! An Error Occurred.... Try again later.......")
+            if (e.hasOwnProperty('responseJSON')) {
+                $searchBar.val(e.responseJSON.error) 
+            }
+            else {
+                console.warn(e)
+                $searchBar.val("OOpsss!!!! An Error Occurred.... Try again later.......")  
+            }
             setTimeout(() => {
                 $searchBar.removeClass("text-red-500 font-bold")
                 $(".search-div").removeClass("red_shadow")
@@ -120,29 +185,50 @@ $(document).on("click", ".toggle-project-modal", function(e) {
 })
 
 
+// Filter Project by Status
+.on("change", "#filter_project", function(e) {
+    $selector = $(this)
 
+    $.ajax ({
+        type: "GET",
+        url: `/?percentage=${$selector.val()}`,
 
+        success: (htmlData) => {
+            $('#result_container').html(htmlData)
+        },
 
+        error: (e) => {
+            console.log(e)
+            $selector.addClass("text-red-500 red_shadow").text("Error")
+            setTimeout(() => {
+                $selector.removeClass("text-red-500 red_shadow").val("All").text("All")
+            },5000)
+        },
+    })
+})
+
+// Edit Project
 .on("click", "#save-edit", function(e){
     const parent_id = $(this).data("parent_id")
     const newTitle = $(`#${parent_id} #editname`).val()
     const newImg = $(`#${parent_id} input[type=file]`).prop("files")[0]
+    const $parent = $(`#${parent_id}`)
     const target = e.target;
-    const formData = new FormData() 
-    if(!newTitle && !newImg){closeEdit(target); return ;}
+    const formData = new FormData()
+    if(!newTitle.length && !newImg){closeEdit(target); return;}
     
-    if (newTitle){formData.append("title", newTitle.trim())}
+    if (newTitle.length){formData.append("title", newTitle.trim())}
     if (newImg){formData.append("image", newImg)}
 
     $.ajax({
-        type: "PUT",
+        type: "POST",
         url: $(this).data("url"),
         data: formData,
         processData: false,  // Prevent jQuery from processing data
         contentType: false,
 
-        success: (data) => {
-            console.log("success")
+        success: (htmlData) => {
+            $parent.html(htmlData)
         },
 
         error: (e) => {
@@ -154,9 +240,7 @@ $(document).on("click", ".toggle-project-modal", function(e) {
 
 })
 
-
-
-
+// Delete Project
 function deleteParent (element) {
     const parent_id = element.getAttribute("data-parentid")
     const $parent = $(`#${parent_id}`)
@@ -176,11 +260,7 @@ function deleteParent (element) {
     })
 }
 
-
-
-
-
-
+// Cancel Edit 
 function closeEdit(element) {
     const parent_id = element.getAttribute("data-parent_id")
     const $edits = $(`#${parent_id} .edit`)
@@ -190,7 +270,7 @@ function closeEdit(element) {
     $edits.css("display", "none")
 }
 
-
+// Edit Project Command
 function editParent (element) {
     const parent_id = element.getAttribute("data-parentid")
     const $edits = $(`#${parent_id} .edit`)
@@ -199,7 +279,7 @@ function editParent (element) {
     $edits.css("display", "flex")
 }
 
-
+// Image Previewer Helper Function
 function previewImage(input, previewTargetId) {
     const $preview = $(previewTargetId);
     $preview.removeClass("hidden")
@@ -215,7 +295,7 @@ function previewImage(input, previewTargetId) {
     }
 }
 
-
+//  Display Error Helper Function
 function displayError(id) {
     const $parent = $(`#${id}`)
     const $errorDisplay = $(`#${id} #error_display`)
@@ -226,4 +306,5 @@ function displayError(id) {
         $parent.removeClass("red_shadow")
         $errorDisplay.css("display", "none")
     },5000)
-}   
+}
+
